b641d6e877bcdc3ba10559926ca2f6c7ae05ddea
Nikos Grammatikos <nikosgram@windowslive.com>
2016-01-30 11:34:07 +0200

    Fix some bugs to home and to channel Pages.

--- asset/js/nikosgram.js
+++ asset/js/nikosgram.js
@@ -1,137 +1,159 @@
-$(function() {
-  var _ID = ['AIzaSyD2OyY85uKKWhHQpveTUwDC8ITbMglJ4ts'];
-  var _SCOPE = ['https://www.googleapis.com/youtube/v3/'];
-  var results = $('[data-results]');
-  var search = $('[data-search]');
+/*!
+ *  License - http://nikosgram13.github.io/LICENSE
+ */
+$(function () {
+    var _ID = ['AIzaSyD2OyY85uKKWhHQpveTUwDC8ITbMglJ4ts', 'WnuWViECtdeuK7hS4qZwbRDxBAU9Kz6pQSxRMpoD'];
+    var _SCOPE = ['https://www.googleapis.com/youtube/v3/'];
+    var results = $('[data-results]');
+    var search = $('[data-search]');
 
-  var canceled = false;
+    var canceled = false;
 
-  function updateHashList() {
-    canceled = false;
-    results.empty();
+    function updateHashList() {
+        canceled = false;
+        results.empty();
 
-    if ($.exsHash('search')) {
+        if (exsHash('search')) {
+            var a = getHash('search');
 
-      results.html(
-        '<div class="col-md-12 channel-item" data-loading>' +
-        '<div class="panel panel-default channel-item">' +
-        '<div class="panel-body">' +
-        '<div class="loading-cube-grid step">' +
-        '<div class="loading-cube loading-cube1"></div>' +
-        '<div class="loading-cube loading-cube2"></div>' +
-        '<div class="loading-cube loading-cube3"></div>' +
-        '<div class="loading-cube loading-cube4"></div>' +
-        '<div class="loading-cube loading-cube5"></div>' +
-        '<div class="loading-cube loading-cube6"></div>' +
-        '<div class="loading-cube loading-cube7"></div>' +
-        '<div class="loading-cube loading-cube8"></div>' +
-        '<div class="loading-cube loading-cube9"></div>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '</div>'
-      );
-
-      $.getJSON(_SCOPE[0] + 'search?maxResults=10&part=snippet&type=channel&q=' + $.getHash('search') + '&key=' + _ID[0], function(returned) {
-        if (canceled) {
-          return;
-        }
-
-        if (returned.items.length) {
-          $.each(returned.items, function() {
-            var item = this;
-            $.getJSON(_SCOPE[0] + 'channels?part=brandingSettings&id=' + this.id.channelId + '&key=' + _ID[0], function(branding) {
-              if (canceled) {
-                return;
-              }
-
-              var channel = item.id.channelId;
-
-              if (item.snippet.channelTitle.trim().length) {
-                channel = item.snippet.channelTitle;
-              }
-
-              var back = -1;
-
-              var background = '';
-              if (branding.items[0].brandingSettings.image.bannerMobileImageUrl) {
-                back = 0;
-                background = 'style="background-image: url(\'' + branding.items[0].brandingSettings.image.bannerMobileImageUrl + '\')"';
-              }
-
-              if (back != -1) {
-                if (item.snippet.description.trim().length) {
-                  back = 0;
-                } else {
-                  back = 1;
-                }
-              }
-
-              $('[data-loading]').parent().empty();
-
-              results.append(
-                '<div class="col-md-12 channel-item" data-channel="' + channel + '">' +
-                '<div class="panel panel-default channel-item" ' + background + '>' +
+            results.html(
+                '<div class="col-md-12 channel-item" data-loading>' +
+                '<div class="panel panel-default channel-item">' +
                 '<div class="panel-body">' +
-                '<div class="media">' +
-                '<div class="media-left">' +
-                '<img class="media-object" src="' + item.snippet.thumbnails.default.url + '" alt="' + item.snippet.title + '">' +
-                '</div>' +
-                '<div class="media-body ' + (back == 0 ? 'texted' : '') + '">' +
-                '<h4 class="media-heading ' + (back == 1 ? 'texted' : '') + '">' + item.snippet.title + '</h4>' +
-                item.snippet.description +
-                '</div>' +
+                '<div class="loading-cube-grid step">' +
+                '<div class="loading-cube loading-cube1"></div>' +
+                '<div class="loading-cube loading-cube2"></div>' +
+                '<div class="loading-cube loading-cube3"></div>' +
+                '<div class="loading-cube loading-cube4"></div>' +
+                '<div class="loading-cube loading-cube5"></div>' +
+                '<div class="loading-cube loading-cube6"></div>' +
+                '<div class="loading-cube loading-cube7"></div>' +
+                '<div class="loading-cube loading-cube8"></div>' +
+                '<div class="loading-cube loading-cube9"></div>' +
                 '</div>' +
                 '</div>' +
                 '</div>' +
                 '</div>'
-              );
+            );
+
+            $.getJSON(_SCOPE[0] + 'search?maxResults=10&part=snippet&type=channel&q=' + a + '&key=' + _ID[0], function (returned) {
+                if (canceled) {
+                    return;
+                }
+
+                if (returned.items.length) {
+                    var items = [];
+
+                    returned['items'].forEach(function (item) {
+                        var snippet = item['snippet'];
+
+                        items.push({
+                            id: item['id']['channelId'],
+                            title: snippet['title'],
+                            desc: snippet['description'],
+                            logo: snippet['thumbnails']['default']['url']
+                        });
+                        $('[data-loading]').parent().empty();
+                    });
+
+                    items.forEach(function (channel) {
+                        results.append(
+                            '<div class="col-md-12 channel-item" data-channel="' + channel.id + '">' +
+                            '<div class="panel panel-default channel-item">' +
+                            '<div class="panel-body">' +
+                            '<div class="media">' +
+                            '<div class="media-left">' +
+                            '<img class="media-object" src="' + channel.logo + '" alt="' + channel.title + '">' +
+                            '</div>' +
+                            '<div class="media-body">' +
+                            '<h4 class="media-heading">' + channel.title + '</h4>' +
+                            channel.desc +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>'
+                        );
+
+                        $.getJSON(_SCOPE[0] + 'channels?part=brandingSettings&id=' + channel.id + '&key=' + _ID[0], function (branding) {
+                            if (canceled) {
+                                return;
+                            }
+
+                            var back = -1;
+
+                            var brand = branding['items'][0]['brandingSettings'];
+
+                            var background = '';
+                            if (brand['image']['bannerMobileImageUrl']) {
+                                back = 0;
+                                background = 'background-image: url(\'' + brand['image']['bannerMobileImageUrl'] + '\')';
+                            }
+
+                            if (back != -1) {
+                                if (channel.desc.trim().length) {
+                                    back = 0;
+                                } else {
+                                    back = 1;
+                                }
+                            }
+
+                            var chan = $('[data-channel="' + channel.id + '"]');
+
+                            chan.find('.channel-item').attr('style', background);
+
+                            if (back == 0) {
+                                chan.find('.media-body').addClass('texted');
+                            } else {
+                                chan.find('.media-heading').addClass('texted');
+                            }
+                        });
+                    });
+                    results.fadeIn('slow');
+                }
             });
-          });
-          results.fadeIn('slow');
+            search.val(a);
         }
-      });
     }
-  }
 
-  $(document).on('click', '[data-channel]', function() {
-    var element = $(this);
-    var channel = element.attr('data-channel');
+    $(document).on('click', '[data-channel]', function () {
+        var element = $(this);
+        var channel = element.attr('data-channel');
 
-    $('body').fadeOut('slow', function() {
-      window.location.href = '/channel/#youtube=' + channel;
+        $('body').fadeOut('slow', function () {
+            window.location.href = 'channel/#youtube=' + channel;
+        });
     });
-  });
 
-  results.bind("DOMSubtreeModified", function() {
-    if ($.trim($(this).html()).length) {
-      if (search.hasClass('search-vec')) {
-        search.removeClass('search-vec');
-      }
-    } else {
-      if (!search.hasClass('search-vec')) {
-        search.addClass('search-vec');
-      }
-    }
-  });
-
-  search.on('input', function() {
-    var element = this;
-    var value = this.value;
-    setTimeout(function() {
-      if (element.value == value) {
-        if (!value.trim()) {
-          $.remHash('search');
+    results.bind("DOMSubtreeModified", function () {
+        if ($.trim($(this).html()).length) {
+            if (search.hasClass('search-vec')) {
+                search.removeClass('search-vec');
+            }
         } else {
-          $.addHash('search', value.trim().replace(/ /g, '+'));
+            if (!search.hasClass('search-vec')) {
+                search.addClass('search-vec');
+            }
         }
-      }
-    }, 350);
-  });
+    });
 
-  $(window).on('hashchange', function() {
+    search.on('input', function () {
+        var element = this;
+        var value = this.value;
+        setTimeout(function () {
+            if (element.value == value) {
+                if (!value.trim()) {
+                    remHash('search');
+                } else {
+                    addHash('search', value.trim().replace(/ /g, '+'));
+                }
+            }
+        }, 350);
+    });
+
+    $(window).on('hashchange', function () {
+        updateHashList();
+    });
+
     updateHashList();
-  });
-
-  updateHashList();
 });
--- channel/asset/js/nikosgram.js
+++ channel/asset/js/nikosgram.js
@@ -1,156 +1,146 @@
-$(function() {
-  var _ID = ['AIzaSyD2OyY85uKKWhHQpveTUwDC8ITbMglJ4ts'];
-  var _SCOPE = ['https://www.googleapis.com/youtube/v3/'];
-  var _USERNAME = ['nikosgram13', 'nikosgram13'];
-  var _PAGE = null;
-  var _HASH = [];
-  var _UPLOAD = [];
+/*!
+ *  License - http://nikosgram13.github.io/LICENSE
+ */
+$(function () {
+    var _ID = ['AIzaSyD2OyY85uKKWhHQpveTUwDC8ITbMglJ4ts'];
+    var _SCOPE = ['https://www.googleapis.com/youtube/v3/'];
+    var _USERNAME = ['nikosgram13', 'nikosgram13'];
+    var _PAGE;
+    //var _HASH = [];
+    var _UPLOAD = [];
 
-  updateHashList();
-
-  function updateHashList() {
-    if ($.exsHash('youtube')) {
-      _USERNAME[0] = $.getHash('youtube');
-    }
-    if ($.exsHash('twitch')) {
-      _USERNAME[1] = $.getHash('twitch');
-    }
-    if ($.exsHash('page')) {
-      _PAGE = $.getHash('page');
-    }
-  }
-
-  function updateVideoList(updatePlayer) {
-    if (_UPLOAD.length <= 0) return;
-    var lvpi = $('[data-lvpi]');
-
-    lvpi.fadeOut('slow', function() {
-      lvpi.html('');
-
-      var page = _PAGE != null ? '&pageToken=' + _PAGE : '';
-
-      $.getJSON(_SCOPE[0] + 'playlistItems?part=snippet&maxResults=3&playlistId=' + _UPLOAD[0] + '&key=' + _ID[0] + page, function(object) {
-        var lvpc = $('[data-lvpc]');
-        var player = $('[data-player]');
-        var prototype = $('[data-lvpo]');
-
-
-        if (object.hasOwnProperty("prevPageToken")) {
-          lvpi.append('<div class="col-md-12 up-arr"><i class="fa fa-chevron-up" style="display: none;" data-page="' + object.prevPageToken + '"></i></div>');
-        }
-
-        var videos = object.items;
-        $.each(videos, function() {
-          lvpi.append(
-            '<div class="col-md-12 video-item" data-video="' + this.snippet.resourceId.videoId + '">' +
-            '<img class="img-responsive b-lazy" src="' + this.snippet.thumbnails.medium.url + '" alt="' + this.snippet.title + '">' +
-            '</div>'
-          );
-        });
-
-        if (object.hasOwnProperty("nextPageToken")) {
-          lvpi.append('<div class="col-md-12 down-arr"><i class="fa fa-chevron-down" style="display: none;" data-page="' + object.nextPageToken + '"></i></div>');
-        }
-
-        if (updatePlayer) {
-          var latest_video = videos[0].snippet.resourceId.videoId;
-
-          player.append('<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + latest_video + '?rel=0&controls=0" frameborder="0" allowfullscreen>');
-          $('#latest_video').focus();
-          lvpc.hide();
-          lvpc.removeClass('sr-only');
-          $('[data-loading]').fadeOut('slow', function() {
-            lvpc.fadeIn('slow');
-          });
-        }
-
-        lvpi.fadeIn('slow');
-      });
-    });
-  }
-
-  $(window).on('hashchange', function() {
-    var oldYoutube = _USERNAME[0];
-    var oldTwitch = _USERNAME[1];
     updateHashList();
 
-    if (_USERNAME[0] != oldYoutube || _USERNAME[1] != oldTwitch) {
-      location.reload();
+    function updateHashList() {
+        if (exsHash('youtube')) {
+            _USERNAME[0] = getHash('youtube');
+        }
+        if (exsHash('twitch')) {
+            _USERNAME[1] = getHash('twitch');
+        }
+        if (exsHash('page')) {
+            _PAGE = getHash('page');
+        }
     }
-  });
 
-  $.getJSON(_SCOPE[0] + 'channels?part=contentDetails&forUsername=' + _USERNAME[0] + '&key=' + _ID[0], function(object) {
-    if (object.items.length <= 0) {
-      $.getJSON(_SCOPE[0] + 'channels?part=contentDetails&id=' + _USERNAME[0] + '&key=' + _ID[0], function(newObject) {
-        if (newObject.items.length <= 0) {
-          alert('I can\'t find this channel in youtube.com.');
-          return;
+    function updateVideoList(updatePlayer) {
+        if (_UPLOAD.length <= 0) return;
+        var lvpi = $('[data-lvpi]');
+
+        lvpi.fadeOut('slow', function () {
+            lvpi.html('');
+
+            var page = _PAGE ? '&pageToken=' + _PAGE : '';
+
+            $.getJSON(_SCOPE[0] + 'playlistItems?part=snippet&maxResults=3&playlistId=' + _UPLOAD[0] + '&key=' + _ID[0] + page, function (object) {
+                var lvpc = $('[data-lvpc]');
+                var player = $('[data-player]');
+                //var prototype = $('[data-lvpo]');
+
+                if (object['prevPageToken']) {
+                    lvpi.append('<div class="col-md-12 up-arr"><i class="arr fa fa-chevron-up" data-page="' + object['prevPageToken'] + '"></i></div>');
+                } else {
+                    lvpi.append('<div class="col-md-12 up-arr"><i class="arr fa fa-chevron-up" style="color:firebrick;"></i></div>');
+                }
+
+                var videos = object['items'];
+                videos.forEach(function (video) {
+                    var snippet = video['snippet'];
+
+                    lvpi.append(
+                        '<div class="col-md-12 video-item" data-video="' + snippet['resourceId']['videoId'] + '">' +
+                        '<img class="img-responsive b-lazy" src="' + snippet['thumbnails']['medium']['url'] + '" alt="' + snippet['title'] + '">' +
+                        '</div>'
+                    );
+                });
+
+                if (object["nextPageToken"]) {
+                    lvpi.append('<div class="col-md-12 down-arr"><i class="arr fa fa-chevron-down" data-page="' + object['nextPageToken'] + '"></i></div>');
+                }
+
+                if (updatePlayer) {
+                    var latest_video = videos[0]['snippet']['resourceId']['videoId'];
+
+                    player.append('<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + latest_video + '?rel=0&controls=0" frameborder="0" allowfullscreen>');
+                    $('#latest_video').focus();
+                    lvpc.hide();
+                    lvpc.removeClass('sr-only');
+                    $('[data-loading]').fadeOut('slow', function () {
+                        lvpc.fadeIn('slow');
+                    });
+                }
+
+                lvpi.fadeIn('slow');
+            });
+        });
+    }
+
+    $(window).on('hashchange', function () {
+        var oldYoutube = _USERNAME[0];
+        var oldTwitch = _USERNAME[1];
+        updateHashList();
+
+        if (_USERNAME[0] != oldYoutube || _USERNAME[1] != oldTwitch) {
+            location.reload();
+        }
+    });
+
+    $.getJSON(_SCOPE[0] + 'channels?part=contentDetails&forUsername=' + _USERNAME[0] + '&key=' + _ID[0], function (object) {
+        if (object['items'].length <= 0) {
+            $.getJSON(_SCOPE[0] + 'channels?part=contentDetails&id=' + _USERNAME[0] + '&key=' + _ID[0], function (newObject) {
+                if (newObject['items'].length <= 0) {
+                    alert('I can\'t find this channel in youtube.com.');
+                    return;
+                }
+
+                var item = newObject['items'][0];
+
+                _UPLOAD[0] = item['contentDetails']['relatedPlaylists']['uploads'];
+                var channelID = item['id'];
+
+                $.getJSON(_SCOPE[0] + 'channels?part=brandingSettings&id=' + channelID + '&key=' + _ID[0], function (brandingSettings) {
+                    $('body').css('background-image', 'url("' + brandingSettings['items'][0]['brandingSettings']['image']['bannerTvHighImageUrl'] + '")');
+                });
+                $.getJSON(_SCOPE[0] + 'channels?part=snippet&id=' + channelID + '&key=' + _ID[0], function (channel) {
+                    $('title').html(channel['items'][0]['snippet']['title']);
+                });
+
+                updateVideoList(true);
+            });
+            return;
         }
 
-        _UPLOAD[0] = newObject.items[0].contentDetails.relatedPlaylists.uploads;
-        var channelID = newObject.items[0].id;
+        var item = object['items'][0];
 
-        $.getJSON(_SCOPE[0] + 'channels?part=brandingSettings&id=' + channelID + '&key=' + _ID[0], function(brandingSettings) {
-          var brandingSettings = brandingSettings.items[0].brandingSettings;
-          $('body').css('background-image', 'url("' + brandingSettings.image.bannerTvHighImageUrl + '")');
+        _UPLOAD[0] = item['contentDetails']['relatedPlaylists']['uploads'];
+        var channelID = item['id'];
+
+        $.getJSON(_SCOPE[0] + 'channels?part=brandingSettings&id=' + channelID + '&key=' + _ID[0], function (brandingSettings) {
+            $('body').css('background-image', 'url("' + brandingSettings['items'][0]['brandingSettings']['image']['bannerTvHighImageUrl'] + '")');
         });
-        $.getJSON(_SCOPE[0] + 'channels?part=snippet&id=' + channelID + '&key=' + _ID[0], function(channel) {
-          var snippet = channel.items[0].snippet;
-
-          $('title').html(snippet.title);
-          $('[data-icon]').attr('href', snippet.thumbnails.default.url);
+        $.getJSON(_SCOPE[0] + 'channels?part=snippet&id=' + channelID + '&key=' + _ID[0], function (channel) {
+            $('title').html(channel['items'][0]['snippet']['title']);
         });
 
         updateVideoList(true);
-      });
-      return;
-    }
-
-    _UPLOAD[0] = object.items[0].contentDetails.relatedPlaylists.uploads;
-    var channelID = object.items[0].id;
-
-    $.getJSON(_SCOPE[0] + 'channels?part=brandingSettings&id=' + channelID + '&key=' + _ID[0], function(brandingSettings) {
-      var brandingSettings = brandingSettings.items[0].brandingSettings;
-      $('body').css('background-image', 'url("' + brandingSettings.image.bannerTvHighImageUrl + '")');
-    });
-    $.getJSON(_SCOPE[0] + 'channels?part=snippet&id=' + channelID + '&key=' + _ID[0], function(channel) {
-      var snippet = channel.items[0].snippet;
-
-      $('title').html(snippet.title);
-      $('[data-icon]').attr('href', snippet.thumbnails.default.url);
     });
 
-    updateVideoList(true);
-  });
+    $(document).on('click', '[data-video]', function () {
+        var element = $(this);
+        var video = element.attr('data-video');
+        var player = $('[data-player]');
 
-  $(document).on('click', '[data-video]', function() {
-    var element = $(this);
-    var video = element.attr('data-video');
-    var player = $('[data-player]');
+        element.focus();
 
-    element.focus();
-
-    player.find('iframe').fadeOut('slow', function() {
-      player.empty();
-      player.append('<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + video + '?rel=0&controls=0&autoplay=1" frameborder="0" allowfullscreen>');
+        player.find('iframe').fadeOut('slow', function () {
+            player.empty();
+            player.append('<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + video + '?rel=0&controls=0&autoplay=1" frameborder="0" allowfullscreen>');
+        });
     });
-  });
 
-  $(document).on("mouseenter", '[data-lvpi]', function() {
-    $.each($('[data-page]'), function() {
-      $(this).fadeIn('fast');
+    $(document).on('click', '[data-page]', function () {
+        addHash('page', $(this).attr('data-page'));
+
+        updateVideoList(false);
     });
-  });
-
-  $(document).on("mouseleave", '[data-lvpi]', function() {
-    $.each($('[data-page]'), function() {
-      $(this).fadeOut('fast');
-    });
-  });
-
-  $(document).on('click', '[data-page]', function() {
-    $.addHash('page', $(this).attr('data-page'));
-
-    updateVideoList(false);
-  });
 });
